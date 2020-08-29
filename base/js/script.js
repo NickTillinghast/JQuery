f2d175941d1936022ba2fa846ef781a877d6c03f
Nick Tillinghast <nickt2@cox.net>
2020-08-28 22:49:37 -0600

    lesson 15

--- base/js/script.js
+++ base/js/script.js
@@ -8,8 +8,54 @@
 //   $(".blue-box").fadeToggle();
 //   $(".blue-box").fadeToggle();
 // });
+// $(function () {
+// $(".blue-box").slideToggle(2000);
+// $(".blue-box").slideDown(2000);
+// $("p").hide();
+// $("p").slideDown(2000);
+
+// });
+// $(function () {
+//   $(".blue-box").animate({
+//     "margin-left": "+=200px",
+
+//   }, 1000, "linear");
+//   $(".blue-box").animate({
+//     "margin-left": "-=200px",
+
+//   }, 1000, "linear");
+
+// });
+// $(function () {
+//   $(".blue-box").animate({
+//     marginLeft: "200px",
+//     opacity: "0",
+//     height: "50px",
+//     "width": "50px",
+//     marginTop: "25px"
+//   }, 1000);
+
+// });
+// $(function () {
+//   $("p").animate({
+//     fontSize: "20px"
+//   }, 1000);
+
+// });
+// $(function () {
+//   $(".red-box").fadeTo(1000, 0.2);
+//   $(".green-box").delay(1000).fadeTo(1000, 0.5);
+//   $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(1000).fadeIn(3000);
+
+
+// // });
 $(function () {
-  $(".blue-box").hide(1000);
-  $(".blue-box").show(1000);
+  $(".red-box").fadeTo(1000, 0, function () {
+    $(".green-box").fadeTo(1000, 0, function () {
+      $(".blue-box").fadeTo(1000, 0);
+    });
+  });
+  // not so nice for clean code.  
+
 
 });
