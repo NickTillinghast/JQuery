b3e3f5f6170ed0f7c61d10cdb2f78d2482d96c55
Nick Tillinghast <nickt2@cox.net>
2020-09-08 22:16:24 -0600

    changing classes using jquery

--- base/js/script.js
+++ base/js/script.js
@@ -1,6 +1,8 @@
 // $(function () {
 //   //   jQuery goes here...
 
+// const { red } = require("color-name");
+
 //   // Uncomment this line to fade out the red box on page load
 //   $(".red-box").fadeTo(3000, 0.2);
 //   $(".green-box").fadeTo(3000, 0.5);
@@ -156,22 +158,64 @@
 
 // })
 //=====================image slideshow===========================
+// $(function () {
+//   var galleryImage = $(".gallery").find("img").first();
+//   var images = [
+//     "images/laptop-mobile_small.jpg",
+//     "images/laptop-on-table_small.jpg",
+//     "images/people-office-group-team_small.jpg",
+//   ];
+//   var i = 0;
+//   setInterval(function () {
+//     i = (i + 1) % images.length;
+//     galleryImage.fadeOut(function () {
+//       $(this).attr("src", images[i]);
+//       $(this).fadeIn();
+//     })
+//     console.log(galleryImage.attr("src"));
+//   }, 3000);
+
+// })
+
+//====================================================================
+
+// $(function () {
+
+//   $(".gallery").css("display", "none");
+
+//   var redBox = $(".red-box");
+//   console.log(redBox.css("width"));
+//   console.log(redBox.width());
+
+//   redBox.css("background-color", "#AA7700");
+//   $("p").css("font-size", "18px");
+//   redBox.css('width', "+=20px");
+
+//   var properties = $('p').css(["font-size", "line-height", "color"])
+//   console.log(properties);
+
+//   redBox.css("user-select", function() {
+//     return "none"
+//   });
+
+// })
+
 $(function () {
-  var galleryImage = $(".gallery").find("img").first();
-  var images = [
-    "images/laptop-mobile_small.jpg",
-    "images/laptop-on-table_small.jpg",
-    "images/people-office-group-team_small.jpg",
-  ];
-  var i = 0;
-  setInterval(function () {
-    i = (i + 1) % images.length;
-    galleryImage.fadeOut(function () {
-      $(this).attr("src", images[i]);
-      $(this).fadeIn();
-    })
-    console.log(galleryImage.attr("src"));
-  }, 3000);
+  // $("a").addClass("fancy-link");
+  // $("p:first").addClass("large emphasize");
+
+  // $("li li").addClass(function (index) {
+  //   $(this).addClass("item-" + index);
+  // });
+
+  // $("div").addClass(function (index, currentClass) {
+  //   if (currentClass === "dummy") {
+  //     return "red-box";
+  //   }
+  // })
+  // $(".red-box").removeClass("red-box").addClass("blue-box");
+  $(".dummy").removeClass("dummy").addClass("green-box");
+
+
 
 })
-
