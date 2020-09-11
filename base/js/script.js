0c63864f369758415cd6ac038e410eedecf1e121
Nick Tillinghast <nickt2@cox.net>
2020-09-10 21:05:26 -0600

    changing classes using jquery

--- base/js/script.js
+++ base/js/script.js
@@ -201,21 +201,22 @@
 // })
 
 $(function () {
-  // $("a").addClass("fancy-link");
-  // $("p:first").addClass("large emphasize");
+  $("a").addClass("fancy-link");
+  $("p:first").addClass("large emphasize");
 
-  // $("li li").addClass(function (index) {
-  //   $(this).addClass("item-" + index);
-  // });
+  $("li li").addClass(function (index) {
+    $(this).addClass("item-" + index);
+  });
 
-  // $("div").addClass(function (index, currentClass) {
-  //   if (currentClass === "dummy") {
-  //     return "red-box";
-  //   }
-  // })
-  // $(".red-box").removeClass("red-box").addClass("blue-box");
+  $("div").addClass(function (index, currentClass) {
+    if (currentClass === "dummy") {
+      return "red-box";
+    }
+  })
+  $(".red-box").removeClass("red-box").addClass("blue-box");
   $(".dummy").removeClass("dummy").addClass("green-box");
 
 
 
 })
+
